8fd638386dc6a132b8caed225967e79318a3a063
unknown <comfortojo21@gmail.com>
2024-01-25 04:26:31 -0800

    first-commit

---
+++ Conditioner.js
@@ -0,0 +1,65 @@
+let userAge = prompt("Enter your age");
+console.log(typeof userAge);
+if (userAge >= 18) {
+    alert("you are old enough to drive")
+} else {
+    let ageLeft =18 - userAge;
+    alert('you are left with ${ageLeft} years to drive')
+}
+
+const number=prompt('what number do you wish to know if it is even or odd?');
+
+
+const myFirstArray = Array(10).fill('students');
+console.log(myFirstArray);
+const studentArray =["aisha", "salamah", "rukayat", "nafisat", "aminat","sukurah" ]
+console.log(studentArray);
+const numberArray= [3,4,5, "men", "bolanle"]
+console.log(mixedArray);
+
+const javascript = 'javascript is bae'
+console.log(javascript);
+console.log(javascript.split("a"))
+
+console.log(studentArray[3]);
+console.log(studentArray.length);
+console.log(javascript.lastIndexOf(a));
+console.log(javascript.lastIndexOf(y));
+
+// ADDS TO THE BACK
+studentArray.push('titilayo');
+console.log(studentArray);
+
+// ADDS TO THE FRONT
+studentArray.unshift('sukurat')
+console.log(studentArray);
+
+// REMOVES FROM THE BACK
+studentArray.pop('titilayo');
+console.log[0]= "mariam";
+console.log(studentArray);
+console.log(studentArray.reverse());
+console.log(studentArray.indexOf("aisha"));
+console.log(studentArray[0]);
+console.log(javascript.charAt(0));
+
+console.log(studentArray.concat(numberArray));
+console.log(Array.isArray(javascript));
+console.log(Array.isArray(studentArray));
+console.log(javascript.split(' '));
+console.log(studentArray.join());
+console.log(studentArray.join(' '));
+studentArray.splice(2,2, 'rufaidat', 'balikis')
+console.log(studentArray);
+console.log(studentArray.slice(1,3));
+console.log(studentArray.sort());
+
+
+
+
+
+
+
+
+
+
--- class.js
+++ class.js
@@ -190,7 +190,7 @@ let secondNumber = prompt("Enter number");
 //Regex
 let number = "I love Javascript and i believe you also love javascript";
 let res = /javascript/gi;
-console.log(numBer.match(res))
+console.log(number.match(res))
 
 const sentenc = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
